b6656e7600a2dfe47ca0c1afb23f46508e3434aa
ShuhratHusainov <1032228444@pfur.ru>
2022-12-29 01:55:41 -0800

    labs

---
+++ labs/lab07/calc.asm
@@ -0,0 +1,30 @@
+%include 'in_out.asm'
+SECTION .data
+msg: DB 'Введите X ',0
+rem: DB 'выражение = : ',0
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+mov ecx, x
+mov edx, 80
+call sread
+mov eax,x ; вызов подпрограммы преобразования
+call atoi ; ASCII кода в число, `eax=x`
+xor edx,edx
+mov ebx,9
+mul ebx
+sub eax,8
+mov ebx,8
+div ebx
+mov ebx,eax
+mov eax,rem
+call sprint
+mov eax,ebx
+call iprintLF
+call quit
+
+
